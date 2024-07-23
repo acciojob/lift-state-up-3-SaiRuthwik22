47e84574efe1559ef8db5a593df2827487df4cb0
SaiRuthwik22 <160124679+SaiRuthwik22@users.noreply.github.com>
2024-07-23 10:51:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
 import React from "react";
+import ChildComponent1 from "./ChildComponent1.js"
+import ChildComponent2 from "./ChildComponent2.js"
 import './../styles/App.css';
 
 const App = () => {
+  const [option,setOption] = React.useState('')
+  function handleOption(random){
+    setOption(random)
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+    <ChildComponent1 handleOption={handleOption}/>
+    <ChildComponent2 handleOption={handleOption}/>
     </div>
   )
 }
